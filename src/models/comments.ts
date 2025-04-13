65eaa431ff77e24314c3885917e8e3a23393c560
Jay Subhashbhai Patel <81366309+JaySPatel2962@users.noreply.github.com>
2025-04-13 18:15:58 +0530

    chore: Video Comments table

---
+++ src/models/comments.ts
@@ -0,0 +1,63 @@
+import dotenv from "dotenv";
+import AWS from "aws-sdk";
+
+dotenv.config();
+
+console.log("process.env.REGION: ", process.env.REGION);
+// Configure AWS
+AWS.config.update({
+  region: process.env.REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID ?? "",
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY ?? "",
+  },
+});
+
+const dynamodb = new AWS.DynamoDB();
+
+const params: AWS.DynamoDB.CreateTableInput = {
+  TableName: "VideoComments",
+  KeySchema: [
+    { AttributeName: "videoId", KeyType: "HASH" },
+    { AttributeName: "createdAt", KeyType: "RANGE" },
+  ],
+  AttributeDefinitions: [
+    { AttributeName: "videoId", AttributeType: "S" },
+    { AttributeName: "createdAt", AttributeType: "S" },
+    { AttributeName: "userId", AttributeType: "S" },
+  ],
+  ProvisionedThroughput: {
+    ReadCapacityUnits: 5,
+    WriteCapacityUnits: 5,
+  },
+  GlobalSecondaryIndexes: [
+    {
+      IndexName: "UserIndex",
+      KeySchema: [
+        { AttributeName: "userId", KeyType: "HASH" },
+        { AttributeName: "createdAt", KeyType: "RANGE" },
+      ],
+      Projection: {
+        ProjectionType: "ALL",
+      },
+      ProvisionedThroughput: {
+        ReadCapacityUnits: 5,
+        WriteCapacityUnits: 5,
+      },
+    },
+  ],
+};
+
+async function createTable() {
+  try {
+    const data = await dynamodb.createTable(params).promise();
+    console.log(
+      "✅ Table created successfully:",
+      JSON.stringify(data, null, 2)
+    );
+  } catch (err) {
+    console.error("❌ Error creating table:", err);
+  }
+}
+
+createTable();
