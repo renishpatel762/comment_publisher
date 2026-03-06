2dc1244fc33523d7335b698c09ac3117bacdcb05
renishpatel762 <renishpatel56454@gmail.com>
2026-03-06 16:56:31 +0530

    spring project initialized

---
+++ src/main/java/com/example/comment_publisher/CommentPublisherApplication.java
@@ -0,0 +1,13 @@
+package com.example.comment_publisher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommentPublisherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommentPublisherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/comment_publisher/CommentPublisherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.comment_publisher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommentPublisherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
